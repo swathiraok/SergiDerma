fa8cf351b53f27ea26554cda9fd696ad49e3814e
avengers-code <contact@finchtech.in>
2020-01-04 11:20:06 +0530

    patient list layout

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,8 +1,36 @@
 import React, { Component} from 'react';
 import Logo from '../assets/images/logo.png';
 import '../index.scss';
+import axios from "axios";
 
 class Header extends Component {
+    constructor(){
+        super();
+        this.state ={
+            query: '',
+            results: []
+        }
+    }
+     getInfo = () => {
+    axios.get("http://139.59.3.138:8082/patients/")
+      .then(({ data }) => {
+        this.setState({
+          results: data // MusicGraph returns an object named data, 
+                             // as does axios. So... data.data                             
+        })
+      })
+  }
+  handleInputChange = () => {
+    this.setState({
+      query: this.search.value
+    }, () => {
+      if (this.state.query && this.state.query.length > 1) {
+        if (this.state.query.length % 2 === 0) {
+          this.getInfo()
+        }
+      } 
+    })
+  }
     render() {
         return (
             <div className="headerWrap float-left">
@@ -16,6 +44,13 @@ class Header extends Component {
                     <h3 className="text-center"> SurgiDerma</h3>
                 </div>
                  <div className="col-md-2 float-left">
+                 <form>
+       <input className="form-control"
+         placeholder="Search Patients"
+         ref={input => this.search = input}
+         onChange={this.handleInputChange}
+       />
+       </form>
                 </div>
                 </div>
 
--- client/src/views/Patients.jsx
+++ client/src/views/Patients.jsx
@@ -4,24 +4,30 @@ import "react-table/react-table.css";
 import ReactTable from "react-table";
 
 // table columns
-const columns = [
-  {
-    Header: "Full Name",
-    accessor: "frstNm"
-  },
-  {
-    Header: "Gender",
-    accessor: "gndr"
-  },
-  {
-    Header: "Age",
-    accessor: "dob"
-  }
-];
+// const columns = [
+//   {
+//     Header: "Full Name",
+//     accessor: "frstNm"
+//   },
+//   {
+//     Header: "Gender",
+//     accessor: "gndr"
+//   },
+//   {
+//     Header: "Age",
+//     accessor: "dob"
+//   }
+// ];
+
+// function MyCell({ value, columnProps: { rest: { someFunc } } }) {
+//   return <a href="#" onClick={someFunc}>{value}</a>
+// }
 class Patients extends Component {
   constructor() {
     super();
     this.state = {
+      // query: "",
+      // results: [],
       tableData: [
         {
           frstNm: "",
@@ -30,6 +36,13 @@ class Patients extends Component {
         }
       ]
     };
+    // this.handleEdit=this.handleEdit.bind(this);
+  }
+  handleEdit(e){
+    alert('edit button clicked');
+  }
+  handleDelete(){
+    alert('delete button clicked');
   }
   componentDidMount(e) {
     // get all patients
@@ -40,15 +53,64 @@ class Patients extends Component {
         this.setState({ tableData: data });
       });
   }
+  getInfo = () => {
+    axios.get("http://139.59.3.138:8082/patients/").then(({ data }) => {
+      this.setState({
+        tableData: data 
+      });
+    });
+  };
+  // handleInputChange = () => {
+  //   this.setState(
+  //     {
+  //       query: this.search.value
+  //     },
+  //     () => {
+  //       if (this.state.query && this.state.query.length > 1) {
+  //         if (this.state.query.length % 2 === 0) {
+  //           this.getInfo();
+  //         }
+  //       }
+  //     }
+  //   );
+  // };
   render() {
     const { tableData } = this.state;
     return (
       <div className="container">
         <div className="topspacing">
+         {/* <form>
+          <input
+            placeholder="Search for..."
+            ref={input => (this.search = input)}
+            onChange={this.handleInputChange}
+          />
+        </form> */}
           <h5>Patient List</h5>
           <ReactTable
             data={tableData}
-            columns={columns}
+            columns={[{
+              Header: 'Full Name',
+              accessor: 'frstNm'
+            }, {
+              Header: 'Gender',
+              accessor: 'gndr'
+          }, {
+            Header: 'Age',
+            accessor: 'dob'
+        },{
+              Header: 'Actions',
+              width: 400,
+              Cell: row => (
+                <div>
+                    <button className="btn btn-success btn-block">Start Consultation</button>
+                    <button className="btn btn-success btn-block">Stop Consulation</button>
+                    <button className="btn btn-success btn-block">Add</button>
+                </div>
+            )
+              // Cell: MyCell,
+              // getProps: () => ({ someFunc: () => alert("clicked")})
+          }]}
             defaultPageSize={10}
             pageSizeOptions={[10, 30, 50]}
             showPagination={true}
